6c20b82ec474f95352ec2d0fb87a85e51e867bb7
Moussa <moussa.sagna@hotmail.fr>
2021-02-08 20:05:16 +0100

    ui finish the product page

--- src/screens/ProductDetails.jsx
+++ src/screens/ProductDetails.jsx
@@ -1,18 +1,12 @@
 import React, { useRef } from 'react'
-import { ScrollView, StyleSheet, Text, View, Image, FlatList, Animated } from 'react-native'
-import CarouselDetailScreen from '../components/CarouselDetailScreen'
-import { WIDTH, HEIGTH, COLORS, SIZES } from '../constants/theme'
+import { StyleSheet, Text, View, Image, FlatList, Animated } from 'react-native'
+import { WIDTH, HEIGTH, SIZES } from '../constants/theme'
 
 const IMAGE_HEIGTH = HEIGTH * 0.75
 const DOT_SIZE = 8
 const DOT_SPACING = 8
 const DOT_INDICATOR_SIZE = DOT_SIZE + DOT_SPACING
-//fake image store
-const images = [
-    "https://i.etsystatic.com/15843670/r/il/b19426/2745189342/il_794xN.2745189342_guj2.jpg",
-    "https://i.etsystatic.com/15843670/r/il/288d80/2788736527/il_794xN.2788736527_9mi2.jpg",
-    "https://i.etsystatic.com/15843670/r/il/d35a86/2788736729/il_794xN.2788736729_hyov.jpg"
-]
+
 
 const ProductDetails = ({ navigation, route }) => {
     const item = route.params
